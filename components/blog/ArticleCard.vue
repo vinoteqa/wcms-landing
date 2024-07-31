0c1a629260c8e08278c9c262572b2fa8f0563ef6
Gabriele De Candido <gabrieledecandido@flashbeing.com>
2024-07-31 21:38:11 +0200

    feat(blog): home page structure

---
+++ components/blog/ArticleCard.vue
@@ -0,0 +1,57 @@
+<template>
+    <NuxtLink :to="link">
+        <article class="relative group bg-vinoteqa/5 hover:bg-vinoteqa/25 rounded-3xl">
+            <figure class="w-full aspect-video overflow-hidden rounded-3xl">
+                <img class="object-cover group-hover:scale-105 transition duration-500 cursor-pointer" :src="imgSrc"
+                    :alt="imgAlt" style="width: 100%; height: 100%; object-fit: cover;">
+            </figure>
+
+            <div class="p-6 text-black">
+                <div>{{ printDate(date) }}</div>
+                <h3 class="my-4 text-2xl font-bold">{{ title }}</h3>
+                <p class="text-normal leading-normal text-black/80 line-clamp-2">{{ subtitle }}</p>
+            </div>
+        </article>
+    </NuxtLink>
+</template>
+
+<script>
+export default {
+    props: {
+        link: {
+            type: String,
+            required: true
+        },
+        title: {
+            type: String,
+            required: true
+        },
+        subtitle: {
+            type: String,
+            required: true
+        },
+        imgSrc: {
+            type: String,
+            required: true
+        },
+        imgAlt: {
+            type: String,
+            required: true
+        },
+        date: {
+            type: String,
+            required: true
+        }
+    },
+    methods: {
+        printDate(date) {
+            const dateOtions = {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric',
+            };
+            return new Date(date).toLocaleDateString(this.$i18n.locale, dateOtions);
+        },
+    }
+}
+</script>
---
+++ components/blog/BlogList.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-10 gap-x-4">
+
+        <ArticleCard v-for="article in data" :key="article._path" :link="article._path" :title="article.title" :subtitle="article.excerpt"
+            :imgSrc="article.media" :imgAlt="article.title" :date="article.date" />
+
+    </div>
+    <p v-if="data.length == 0" class="w-full md:w-7/12 text-h3 leading-h3 font-bold dark:text-white">{{ message }}</p>
+</template>
+
+<script setup>
+const props = defineProps({
+    data: {
+        type: Array,
+        required: true
+    },
+    message: {
+        type: String,
+        default: "There are no posts right now, but stay tuned for newer releases in the future."
+    }
+});
+</script>
