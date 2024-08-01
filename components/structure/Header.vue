69705c6309e0247c2db972060d06b1afe79d4c1e
Gabriele De Candido <gabrieledecandido@flashbeing.com>
2024-08-01 17:42:42 +0200

    fix: logo on mobile

--- components/structure/Header.vue
+++ components/structure/Header.vue
@@ -34,7 +34,7 @@
                 <div class="flex items-center justify-between">
                     <NuxtLink :to="localePath(homeLink)" class="-m-1.5 p-1.5">
                         <span class="sr-only">Vinoteqa</span>
-                        <img class="h-8 w-auto" :src="logoSvg" :alt="logoAlt" />
+                        <img class="h-8 w-auto" :src="logoSrc" :alt="logoAlt" />
                     </NuxtLink>
                     <button type="button" class="-m-2.5 rounded-md p-2.5 text-gray-700" @click="mobileMenuOpen = false">
                         <span class="sr-only">Close menu</span>
