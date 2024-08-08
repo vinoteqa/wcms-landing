9bc59827ccc652679953506840c6b67bbc798e72
Gabriele De Candido <gabrieledecandido@flashbeing.com>
2024-08-08 13:00:45 +0200

    feat: add nuxt studio module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,7 +6,13 @@ export default defineNuxtConfig({
 
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ["@nuxtjs/i18n", "@nuxtjs/tailwindcss", "@nuxtjs/seo", "@nuxt/content"],
+  modules: [
+    "@nuxtjs/i18n",
+    "@nuxtjs/tailwindcss",
+    "@nuxtjs/seo",
+    "@nuxt/content",
+    "@nuxthq/studio"
+  ],
 
   runtimeConfig: {
     public: {
