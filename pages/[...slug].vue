ae8ccd2f0cd19535fb08291c3175520369633667
Gabriele De Candido <gabrieledecandido@flashbeing.com>
2024-08-10 17:39:36 +0200

    fix: content page padding

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,13 +1,15 @@
 <template>
-    <ContentDoc>
-        <template v-slot="{ doc }">
-            <Content :doc="doc" />
-        </template>
+    <div class="px-8 mx-auto max-w-7xl">
+        <ContentDoc>
+            <template v-slot="{ doc }">
+                <Content :doc="doc" />
+            </template>
 
-        <template #not-found>
-            <NotFoundError />
-        </template>
-    </ContentDoc>
+            <template #not-found>
+                <NotFoundError />
+            </template>
+        </ContentDoc>
+    </div>
 </template>
 
 <script>
