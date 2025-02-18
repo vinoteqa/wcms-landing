3325f017ced9688c16c8c314f5b073cc08d8b4c5
Gabriele De Candido <gabriele@vintoeqa.com>
2025-02-18 15:18:31 +0100

    feat: middleware page to redirect to demo

---
+++ layouts/white.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="page-ct bg-white">
+        <slot />
+    </div>
+</template>
---
+++ pages/demo.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="text-base text-black">Redirecting...</div>
+</template>
+
+<script setup lang="ts">
+definePageMeta({
+  layout: 'white'
+})
+
+import { useRoute, useRuntimeConfig } from '#imports'
+
+const route = useRoute()
+const config = useRuntimeConfig()
+
+const demoLinks = config.public.demoBookingPageLink as Record<string, string>
+
+// Get language from query parameter
+const lang = (route.query.lang as string) || 'en'
+
+// Get corresponding demo link
+const demoLink = demoLinks[lang] || demoLinks.en
+navigateTo(demoLink, { external: true })
+</script>
